983c0c2709cc63c90a0cbf13412840bcae6cffa7
Zeyd75 <58294767+Zeyd75@users.noreply.github.com>
2021-10-04 09:17:24 +0200

    display nounours via js

--- front-end/assets/js/index.js
+++ front-end/assets/js/index.js
@@ -1,5 +1,35 @@
-fetch("http://localhost:3000/api/teddies")
-  .then((product) => product.json())
-  .then((product) => {
-    console.log(product);
+//Utilisation de la méthode fetch pour établir le lien avec l'API
+fetch('http://localhost:3000/api/teddies')
+  .then(function(response) {
+    if(response.ok) {
+      return response.json();
+    }
+  })
+  .then(function(data) {
+    teddiesDisplay(data);
+  })
+  .catch(function(error) {
+    alert("Connection impossible");
   });
+
+//Fonction permettant d'afficher les produits et leurs caractéristiques
+function teddiesDisplay(data) {
+  let display = '<div class="row teddy">';
+  for (let teddy of data) {
+    display += `<div class="col-sm-12 col-lg-4">
+                  <a href="#" class="teddyLink">
+                    <div class="card">
+                        <img class="card-img-top" src="${teddy.imageUrl}" alt="${teddy.name}">
+                        <div class="row card-body">
+                            <h3 class="card-title col-9">${teddy.name}</h3>
+                            <p class="teddyPrice col-3">${teddy.price}</p>
+                        </div>
+                        <p class="card-test">${teddy.description}</p>
+                    </div>
+                  </a>
+                </div>`;
+  }
+  display += '</div>';
+  document.querySelector('#teddiesList').innerHTML = display;
+};
+
