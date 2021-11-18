da448a3344180fa25088d6450c603ad611c920d4
Zeyd75 <58294767+Zeyd75@users.noreply.github.com>
2021-11-18 19:10:49 +0100

    creation boutton page produit

--- front-end/assets/js/produit.js
+++ front-end/assets/js/produit.js
@@ -1,32 +1,18 @@
-//Récupération id produit
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const teddyId = urlParams.get("id");
+//Récupération de l'ID du produit via l'URL
+let searchParams = new URLSearchParams(window.location.search);
+let teddyId = searchParams.get('id');
 
-
-  fetch(`http://localhost:3000/api/teddies/${teddyId}`)
-  .then(function(response) {
-    if(response.ok) {
-      return response.json();
-    }
-  })
-  .then(function(data) {
-    console.log(data);
-     // teddiesDisplay(data);
-  });
-  // .catch(function(error) {
-  //   alert("Connection impossible");
-  // });
+//Lien vers l'API en ciblant son ID
+fetch(`http://localhost:3000/api/teddies/${teddyId}`)
+.then(function(response) {
+  if(response.ok) {
+    return response.json();
+  }
+})
+.then(function(data) {
+  console.log(data);
+});
 
 function displayTeddy(teddy) {
 
-}
-
-//fonction qui permet d'afficher les différents vernis
-function colorsOptions(data) {
-  const dataOption = document.getElementById("productColor");
-  for (let color of data.color) {
-    dataOption.innerHTML += `<option value="${colors}">${colors}</option>`;
-    console.log(color);
-  }
 }
